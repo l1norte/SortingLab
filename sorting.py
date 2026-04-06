e856d3ab699fb49f3cbafa57f783e1dd27fb5ecb
l1norte <ne4au228666@gmail.com>
2026-04-06 23:41:46 +0300

    Last release

--- sorting.py
+++ sorting.py
@@ -37,7 +37,7 @@ def quick_sort(arr):
 
 
 def run_lab_requirements():
-    print("=== ЗАМІР ЧАСУ: 10 000 елементів ===")
+    print("Timing of 10,000 elements")
     data_10k = [random.randint(0, 100000) for _ in range(10000)]
 
     s1 = time.time()
@@ -48,23 +48,23 @@ def run_lab_requirements():
     quick_sort(data_10k.copy())
     print(f"QuickSort (10k): {time.time() - s2:.4f} сек")
 
-    print("\n=== РЕЗУЛЬТАТИ: 20 000 елементів ===")
+    print("\nResult 20000 elements")
     data_20k = [random.randint(1, 1000000) for _ in range(20000)]
 
-    print("До сортування (перші 20):")
+    print("Before sorting (top 20):")
     print(*(data_20k[:20]))
 
     s3 = time.time()
     sorted_m = merge_sort(data_20k.copy())
-    print("Після MergeSort (перші 20):")
+    print("After MergeSort (the first 20):")
     print(*(sorted_m[:20]))
-    print(f"Час MergeSort (20k): {time.time() - s3:.4f} сек")
+    print(f"MergeSort runtime (20k): {time.time() - s3:.4f} сек")
 
     s4 = time.time()
     sorted_q = quick_sort(data_20k.copy())
-    print("Після QuickSort (перші 20):")
+    print("After QuickSort (the first 20):")
     print(*(sorted_q[:20]))
-    print(f"Час QuickSort (20k): {time.time() - s4:.4f} сек")
+    print(f"QuickSort runtime (20k): {time.time() - s4:.4f} сек")
 
 
 if __name__ == "__main__":
